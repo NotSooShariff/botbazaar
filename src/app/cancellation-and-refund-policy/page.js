5e38b1e2e5c310e2dcbf8f3e85b1802a20b9ce2a
Owais Shariff <osh.econnect@gmail>
2024-05-29 02:53:52 +0300

    Refunds Page Added

---
+++ src/app/cancellation-and-refund-policy/page.js
@@ -0,0 +1,10 @@
+import { Refunds } from '@/components/component/refunds';
+import React from 'react';
+
+export default function RefundsPage() {
+  return (
+    <div>
+      <Refunds/>
+    </div>
+  );
+}
